d198162ddedef9a89687a162f1e0baafa3f1d6c5
giovanna004 <giovannaqs128@gmail.com>
2025-02-04 10:18:52 -0400

    demais funções

---
+++ netflix/netflix.py
@@ -0,0 +1,7 @@
+#Qual perfil é mais usado?
+#Qual o titulo mais acessado?
+#Que dia foi mais acessado?
+#De qual pais tem mais acessos?
+#Maior tempo logado
+#Perfil x: horas de netflix no total
+#Horas no total da conta
--- weblog/analisador.py
+++ weblog/analisador.py
@@ -3,6 +3,8 @@
 #- Quantas requisições com status de erro tiveram?
 #Crie suas próprias perguntas.
 # - Quantos acessos deram certo?
+# - Qual dia teve mais acessos?
+# - Qual horário teve mais acessos?
 import csv
 from rich.console import Console
 from rich.panel import Panel
@@ -41,6 +43,51 @@ def contar_ips(log):
     
     return contagem_ips
 
+def analisar_status(log):
+    contagem_status = {}
+
+    for linha in log:
+        if len(linha) > 1:
+            status = linha[3]
+
+            if status in contagem_status:
+                contagem_status[status] += 1
+            else:
+                contagem_status[status] = 1
+    return contagem_status
+            
+def status_sucessos(contagem_status):
+    contar = 0
+    for i in contagem_status:
+        if i.isdigit() and i != '404':
+            contar = contar + contagem_status[i]
+    return contar
+
+def dia_mais_acessos(log):
+    dias = {}
+    for linha in log:
+        if len(linha) > 2:
+            divisao_linha = linha[1].split()
+            data = divisao_linha[0].replace('[', '')[:11]
+            if data in dias:
+                dias[data] += 1
+            else:
+                dias[data]  = 1
+    dia_mais_acessado = sorted(dias.items(), key=lambda x: x[1], reverse=True)
+    return dia_mais_acessado[0][0]
+
+def hora_mais_acessos(log):
+    horas = {}
+    for linha in log:
+        if len(linha) > 2:
+            divisao_linha = linha[1].split()
+            hora = divisao_linha[0].replace('[', '')[12:14]
+            if hora in horas:
+                horas[hora] += 1
+            else:
+                horas[hora]  = 1
+    hora_mais_acessado = sorted(horas.items(), key=lambda x: x[1], reverse=True)
+    return hora_mais_acessado[0][0]
 
 def main():
     leitor = abrir_log()
@@ -51,12 +98,16 @@ def main():
     print(f"Post: {contagem['POST']}")
 
     contagem_ips = contar_ips(leitor)
-    
     ips_ordenados = sorted(contagem_ips.items(), key=lambda x: x[1], reverse=True)
     
     print("\nTop 5 IPs que mais acessaram:")
     for ip, quantidade in ips_ordenados[:5]:  
         print(f"{ip}: {quantidade} acessos")
-    
+
+    contar_status = analisar_status(leitor)
+    print(f"\nQuantidade de status de erro: {contar_status['404']}")
+    print(status_sucessos(contar_status)) #contagem de sucessos
+    print(dia_mais_acessos(leitor)) #dias mais acessado
+    print(hora_mais_acessos(leitor)) #hora mais acessada
 
 main()
